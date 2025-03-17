62b153686deb3ca00d9cf8b492e3f68978e2a536
lezgea <muslim.ragimov@yahoo.com>
2025-03-16 23:37:53 -0400

    Yandex metrica tracking script has been added

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -32,6 +32,26 @@ export default async function RootLayout({
       <head>
         {/* Link to the favicon */}
         <link rel="icon" href="/favicon.ico" />
+
+        {/* Yandex Metrica Tracking Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+   ym(100402630, "init", {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+              `,
+          }}
+        />
       </head>
       <body className={`${inter.className} ${poppins.className}`}>
         <NextIntlClientProvider messages={messages}>
